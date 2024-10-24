8f3be34987d1e05b4ed86ff26beec0d4bbf9340b
marcelo dominguez <marceelodominguez@gmail.com>
2024-10-24 19:40:19 -0300

    add file authController.ts

---
+++ api/src/routes/auth/authController.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from "express";
+import { usersTable } from "../../db/usersSchema";
+import { db } from "../../db";
+import bcrypt from "bcryptjs";
+import { eq } from "drizzle-orm";
+import jwt from "jsonwebtoken";
+
+export async function register(req: Request, res: Response) {
+  try {
+    const data = req.cleanBody;
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const [user] = await db
+      .insert(usersTable)
+      .values({ ...data, password: hashedPassword })
+      .returning();
+
+    res.status(201).json({ user });
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
+}
+
+export async function login(req: Request, res: Response) {
+  try {
+    const { email, password } = req.cleanBody;
+
+    const [user] = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.email, email));
+
+    if (!user) {
+      res.status(401).json({ error: "Authentication failed" });
+      return;
+    }
+
+    const matched = await bcrypt.compare(password, user.password);
+
+    if (!matched) {
+      res.status(401).json({ error: "Authentication failed" });
+      return;
+    }
+
+    //create jwt token
+    const token = jwt.sign(
+      { userId: user.id, role: user.role },
+      "your-secret",
+      { expiresIn: "48h" }
+    );
+
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
+}
--- api/src/routes/auth/index.ts
+++ api/src/routes/auth/index.ts
@@ -1,65 +1,11 @@
 import { Router } from "express";
-import {
-  createUserSchema,
-  loginSchema,
-  usersTable,
-} from "../../db/usersSchema";
+import { createUserSchema, loginSchema } from "../../db/usersSchema";
 import { validateData } from "../../middlewares/validationMiddleware";
-import bcrypt from "bcryptjs";
-import { db } from "../../db";
-import { eq } from "drizzle-orm";
-import jwt from "jsonwebtoken";
+import { login, register } from "./authController";
 
 const router = Router();
 
-router.post("/register", validateData(createUserSchema), async (req, res) => {
-  try {
-    const data = req.cleanBody;
-    const hashedPassword = await bcrypt.hash(data.password, 10);
-
-    const [user] = await db
-      .insert(usersTable)
-      .values({ ...data, password: hashedPassword })
-      .returning();
-
-    res.status(201).json({ user });
-  } catch (error) {
-    res.status(500).send("Something went wrong");
-  }
-});
-
-router.post("/login", validateData(loginSchema), async (req, res) => {
-  try {
-    const { email, password } = req.cleanBody;
-
-    const [user] = await db
-      .select()
-      .from(usersTable)
-      .where(eq(usersTable.email, email));
-
-    if (!user) {
-      res.status(401).json({ error: "Authentication failed" });
-      return;
-    }
-
-    const matched = await bcrypt.compare(password, user.password);
-
-    if (!matched) {
-      res.status(401).json({ error: "Authentication failed" });
-      return;
-    }
-
-    //create jwt token
-    const token = jwt.sign(
-      { userId: user.id, role: user.role },
-      "your-secret",
-      { expiresIn: "48h" }
-    );
-
-    res.status(200).json({ token, user });
-  } catch (error) {
-    res.status(500).send("Something went wrong");
-  }
-});
+router.post("/register", validateData(createUserSchema), register);
+router.post("/login", validateData(loginSchema), login);
 
 export default router;
