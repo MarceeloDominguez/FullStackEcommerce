07eaecce93e064f26bb706830c28e93633355b38
marcelo dominguez <marceelodominguez@gmail.com>
2024-10-31 12:23:56 -0300

    add gluestack UI

---
+++ ecommerce-app/app/_layout.tsx
@@ -0,0 +1,11 @@
+import "../global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <GluestackUIProvider>
+      <Stack />
+    </GluestackUIProvider>
+  );
+}
--- ecommerce-app/app/index.tsx
+++ ecommerce-app/app/index.tsx
@@ -1,10 +1,20 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Box } from "@/components/ui/box";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View className="bg-violet-600">
+      <Text className="text-red-600">Home Screen</Text>
+
+      <Button size="md" variant="solid" action="negative">
+        <ButtonText>Hello World!</ButtonText>
+      </Button>
+
+      <Box className="bg-green-600 p-5">
+        <Text className="text-red-600">This is the Box</Text>
+      </Box>
     </View>
   );
 }
